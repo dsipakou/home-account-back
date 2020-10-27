7f11f69708d9c2128a63d80d4d134e825217d7bf
Denis Sipakov <d.sipakou@gmail.com>
2020-10-27 19:26:27 +0300

    Update

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = required('express');
+require('dotenv').config();
 var app = express();
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
 
 app.get('/', (request, response) => {
   request.send('Hi there!');
