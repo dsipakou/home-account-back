2290831031b8dd9cd1f6034edf1eb599b0778f2b
Denis Sipakov <d.sipakou@gmail.com>
2020-10-30 17:22:06 +0300

    Update auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const { User } = require('../models/UserModel');
+
+const auth = (request, response, next) => {
+  let token = request.cookies.authToken;
+
+  User.findByToken(token, (error, user) => {
+    if (error) {
+      throw error;
+    }
+    if (!user) {
+      return response.json({ isAuth: false, error: true });
+    }
+    request.token = token;
+    request.user = user;
+    next();
+  });
+}
+
+module.export = { auth }
